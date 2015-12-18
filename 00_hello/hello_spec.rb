6ef01a6ccca25dc408b52b376809753694f35519
Sherri Mitchell <rsamitchell@att.net>
2015-12-17 22:38:28 -0500

    Adding hello, temperature, and gemfile.lock.

--- 00_hello/hello_spec.rb
+++ 00_hello/hello_spec.rb
@@ -117,7 +117,7 @@ require "hello"
 
 describe "the hello function" do
   it "says hello" do
-    hello.should == "Hello!"
+    expect(hello).to eq ("Hello!")
   end
 end
 
---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,10 @@
+def ftoc(temperature)
+	 ftoc == (celsius * 5 / 9) +32
+end
+
+# if original_temperature == 'c'
+#     new_temperature = (current_temperature * 5 / 9) + 32
+#     puts "New temperature: #{new_temperature} F"
+# elsif original_temperature == 'f'
+#     new_temperature = (current_temperature - 32) * 9 / 5
+#     puts "New temperature: #{new_temperature} C"
